a7c4a54393c2c418bbecf29c28d129eae5a69330
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-08-28 10:34:32 +0530

    fixed the landing page to just the video format

--- app/page.js
+++ app/page.js
@@ -1,12 +1,5 @@
-import { getPortfolioPersons, getPersonContent } from "@/lib/content";
 import LandingClient from "@/components/LandingClient";
 
 export default function LandingPage() {
-  const persons = getPortfolioPersons();
-  const profiles = persons.map((p) => ({
-    slug: p,
-    ...getPersonContent(p, "profile"),
-  }));
-
-  return <LandingClient profiles={profiles} />;
+  return <LandingClient />;
 }
